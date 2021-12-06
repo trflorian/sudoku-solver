9630f56b39977d062c8e73dfb962c519c9f981e2
trflorian <florian.trautweiler@hispeed.ch>
2021-12-06 16:19:54 +0100

    renamed data directory, added script for extracting sudoku in examples

--- examples/crop_sudoku.py
+++
@@ -1,7 +0,0 @@
-from visual_sudoku_solver import extractor
-import cv2.cv2 as cv2
-
-# load sudoku picture
-img = cv2.imread('img/sudoku.jpg')
-crop = extractor.sudoku_image(img, 400)
-cv2.imwrite('img/sudoku_cropped.jpg', crop)
---
+++ examples/extract_sudoku_from_image.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from visual_sudoku_solver import extractor
+import cv2.cv2 as cv2
+
+# load sudoku image
+img = cv2.imread('data/image.jpg')
+
+# crop and save
+crop = extractor.sudoku_image(img, 400)
+cv2.imwrite('data/cropped.jpg', crop)
+
+# read digits and save
+digits = extractor.digits_from_sudoku_image(crop)
+np.savetxt('data/digits.txt', digits.astype(int), fmt='%s')
+
+# show cropped sudoku
+cv2.imshow('Sudoku', crop)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
